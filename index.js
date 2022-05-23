1046589db8ab4e03b9a0861054966d5912c99009
FazlulHaqueRazaChowdhury <fozlulcoc1592@gmail.com>
2022-05-23 22:44:42 +0600

    fixed some bugs

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
+
 //middleware
 app.use(cors());
 app.use(express.json());
@@ -75,23 +76,55 @@ async function run() {
                     photoURL: information.photoURL,
                     name: information.name,
                     email: information.email,
-                    street: information.street,
-                    city: information.city,
-                    country: information.country,
-                    zip: information.zip,
-                    phone: information.phone,
                 },
             };
+
             const result = await userCollection.updateOne(query, updateDoc, options);
             return res.send(result);
         }
         return res.status(404).send({ message: 'Something went wrong' });
+    })
+    //updating user on the purchase page and on the my profile
+    app.patch('/users/:email', async (req, res) => {
+
+        const email = req.params.email;
+        console.log(email);
+        const query = {
+            email: email
+        }
+        const information = req.body;
+        const options = { upsert: false };
+        const updateDoc = {
+            $set: {
+                photoURL: information.photoURL,
+                name: information.name,
+                email: information.email,
+                street: information.street,
+                city: information.city,
+                country: information.country,
+                zip: information.zip,
+                phone: information.phone,
+            },
+
+        };
+        const result = await userCollection.updateOne(query, updateDoc, options);
+        res.send(result);
+
     })
     //getting all user 
     app.get('/users', async (req, res) => {
         const query = {};
         const filter = await userCollection.find(query).toArray();
         res.send(filter);
+    })
+    //getting user by email
+    app.get('/users/:email', async (req, res) => {
+        const query = {
+            email: req.params.email
+        };
+        const find = await userCollection.findOne(query);
+
+        res.send(find);
     })
     //adding orders 
     app.post('/orders/', async (req, res) => {
@@ -111,6 +144,22 @@ async function run() {
         const query = {};
         const result = await orderCollection.find(query).toArray();
         res.send(result)
+    })
+    //app.get orders by email 
+    app.get('/orders/:email', async (req, res) => {
+        const query = {
+            email: req.params.email
+        }
+        const find = await orderCollection.find(query).toArray();
+        res.send(find);
+    })
+    //delete a order
+    app.delete('/orders/:id', async (req, res) => {
+        const query = {
+            _id: ObjectId(req.params.id)
+        }
+        const deleting = await orderCollection.deleteOne(query);
+        res.send(deleting);
     })
 }
 run()
