7d9b747bbdfb60c00adee35eea22574114fad405
FazlulHaqueRazaChowdhury <fozlulcoc1592@gmail.com>
2022-05-23 14:04:33 +0600

    after log in or sign up user profile will go to the database

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //importing
 const express = require("express");
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -27,7 +27,7 @@ async function run() {
     client.connect();
     //collection of the product
     const productsCollection = client.db("electoold").collection('products');
-
+    const userCollection = client.db('electoold').collection('users');
 
 
     //getting all products
@@ -36,6 +36,44 @@ async function run() {
         const productLimit = parseInt(req.query.limit);
         const products = await productsCollection.find(query).sort({ _id: -1 }).limit(productLimit).toArray();
         res.send(products);
+    })
+    //getting product by its id
+    app.get('/products/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const filter = await productsCollection.findOne(query);
+        res.send(filter);
+    })
+    //updating a user if logged in and inserting a user if he signed up
+    app.put('/users/:email', async (req, res) => {
+        const email = req.params.email;
+        if (email) {
+            const query = {
+                email: email
+            }
+            const information = req.body;
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    photoURL: information.photoURL,
+                    name: information.name,
+                    email: information.email,
+                    street: information.street,
+                    city: information.city,
+                    country: information.country,
+                    zip: information.zip,
+                },
+            };
+            const result = await userCollection.updateOne(query, updateDoc, options);
+            return res.send(result);
+        }
+        return res.status(404).send({ message: 'Something went wrong' });
+    })
+    //getting all user 
+    app.get('/users', async (req, res) => {
+        const query = {};
+        const filter = await userCollection.find(query).toArray();
+        res.send(filter);
     })
 }
 run()
