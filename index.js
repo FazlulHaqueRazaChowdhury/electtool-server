386a7ac8fbfe049c509b3a5d68e6a0927e063b9b
FazlulHaqueRazaChowdhury <fozlulcoc1592@gmail.com>
2022-05-24 13:42:29 +0600

    added payment

--- index.js
+++ index.js
@@ -2,10 +2,11 @@
 const express = require("express");
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { query } = require("express");
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
-
+const stripe = require("stripe")(process.env.STRIPE_KEY);
 //middleware
 app.use(cors());
 app.use(express.json());
@@ -30,6 +31,7 @@ async function run() {
     const productsCollection = client.db("electoold").collection('products');
     const userCollection = client.db('electoold').collection('users');
     const orderCollection = client.db('electoold').collection('orders');
+    const reviewsCollection = client.db('electoold').collection('reviews');
 
     //getting all products
     app.get('/products', async (req, res) => {
@@ -88,7 +90,7 @@ async function run() {
     app.patch('/users/:email', async (req, res) => {
 
         const email = req.params.email;
-        console.log(email);
+
         const query = {
             email: email
         }
@@ -129,8 +131,8 @@ async function run() {
     //adding orders 
     app.post('/orders/', async (req, res) => {
         const orderInformation = req.body;
-        const find = await orderCollection.findOne({ email: orderInformation.email, productID: orderInformation.productID });
-        console.log(!!find);
+        const find = await orderCollection.findOne({ email: orderInformation.email, productID: orderInformation.productID, paid: false });
+
         if (find) {
             return res.send({ success: false, message: `Already have an order place for the same product` });
         }
@@ -145,7 +147,7 @@ async function run() {
         const result = await orderCollection.find(query).toArray();
         res.send(result)
     })
-    //app.get orders by email 
+    //app get orders by email 
     app.get('/orders/:email', async (req, res) => {
         const query = {
             email: req.params.email
@@ -153,17 +155,77 @@ async function run() {
         const find = await orderCollection.find(query).toArray();
         res.send(find);
     })
+    //app get a order by id
+    app.get('/order/:id', async (req, res) => {
+        const id = req.params.id;
+
+        const query = {
+            _id: ObjectId(id),
+        }
+        const find = await orderCollection.findOne(query);
+        res.send(find);
+
+    })
     //delete a order
     app.delete('/orders/:id', async (req, res) => {
-        console.log(req.params.id);
+
         const id = req.params.id;
         const query = {
             _id: ObjectId(id)
         }
         const deleting = await orderCollection.deleteOne(query);
-        console.log(deleting);
+
         res.send(deleting);
     })
+    //update order from payment
+    app.patch('/order', async (req, res) => {
+        const id = req.body.orderId;
+        const transcitionId = req.body.transcitionId;
+        console.log(transcitionId);
+        const filter = {
+            _id: ObjectId(id)
+        }
+        const option = { upsert: false };
+        const updateDoc = {
+            $set: {
+                paid: true,
+                transcitionId: transcitionId
+            }
+        }
+        const result = await orderCollection.updateOne(filter, updateDoc, option);
+        res.send(result);
+    })
+    //add a review 
+    app.post('/reviews', async (req, res) => {
+        const review = req.body;
+        const upload = await reviewsCollection.insertOne(review);
+        res.send(upload);
+    })
+    //get review
+    app.get('/reviews', async (req, res) => {
+        const query = {};
+        const find = await reviewsCollection.find(query).sort({ _id: -1 }).toArray();
+        res.send(find);
+    })
+    //creating payment intent
+
+    app.post("/create-payment-intent", async (req, res) => {
+        const price = parseInt(req.query.price);
+        const amount = price * 100;
+        console.log(amount);
+        if (amount) {
+            // Create a PaymentIntent with the order amount and currency
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: "usd",
+                payment_method_types: ['card']
+            });
+
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+            });
+        }
+    });
 }
 run()
 
